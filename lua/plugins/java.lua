95721a64fb631a1ce5ce789da2363c2fa90d2f9a
afaqjaved <afaqjavedofficial@gmail.com>
2026-07-18 16:11:34 +0500

    merged

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "nvim-java/nvim-java",
-    config = function()
-      require("java").setup()
-      vim.lsp.enable("jdtls")
-    end,
-  },
-  -- nvim-dap has no setup() function; override to prevent lazy.nvim from
-  -- auto-generating `require('dap').setup(opts)` from the lang.java extra opts
-  {
-    "mfussenegger/nvim-dap",
-    config = function() end,
-  },
+  -- {
+  --   "nvim-java/nvim-java",
+  --   config = function()
+  --     require("java").setup()
+  --     vim.lsp.enable("jdtls")
+  --   end,
+  -- },
+  -- -- nvim-dap has no setup() function; override to prevent lazy.nvim from
+  -- -- auto-generating `require('dap').setup(opts)` from the lang.java extra opts
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   config = function() end,
+  -- },
 }
